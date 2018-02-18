845d2f7ec11ba2b61f88c4391badc847a10a012e
Luiz Santos <luiz.g.s07@gmail.com>
2018-02-18 17:28:55 -0300

    going

---
+++ dev-resources/user.clj
@@ -0,0 +1,30 @@
+(ns user
+  (:require
+   [graphql-playground.schema :as s]
+   [clojure.walk :as walk]
+   [com.walmartlabs.lacinia :as lacinia])
+  (:import (clojure.lang IPersistentMap)))
+
+(def schema (s/load-schema))
+
+(defn simplify
+  "Converts all ordered maps nested within the map into standard hash maps, and
+   sequences into vectors, which makes for easier constants in the tests, and eliminates ordering problems."
+  [m]
+  (walk/postwalk
+   (fn [node]
+     (cond
+       (instance? IPersistentMap node)
+       (into {} node)
+
+       (seq? node)
+       (vec node)
+
+       :else
+       node))
+   m))
+
+(defn q
+  [query-string]
+  (-> (lacinia/execute schema query-string nil nil)
+      simplify))
--- src/graphql_playground/schema.clj
+++ src/graphql_playground/schema.clj
@@ -4,8 +4,21 @@
             [clojure.edn :as edn]
             [clojure.java.io :as io]))
 
-(defn resolver-map []
-  {:query/game-by-id (fn [context args value] nil)})
+(defn resolve-game-by-id
+  [games-map context args value]
+  (let [{:keys [id]} args]
+    (get games-map id)))
+
+(defn resolver-map
+  []
+  (let [cgg-data (-> (io/resource "cgg-data.edn")
+                     slurp
+                     edn/read-string)
+        games-map (->> cgg-data
+                       :games
+                       (reduce #(assoc %1 (:id %2) %2) {}))]
+
+    {:query/game-by-id (partial resolve-game-by-id games-map)}))
 
 (defn load-schema []
   (-> (io/resource "cgg-schema.edn")
